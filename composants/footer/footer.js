01db0bae662b61b019a40cc92b9f441095854a71
ChapeauDent <ncsray@live.fr>
2025-07-10 10:16:40 +0200

    ajout composants footer

---
+++ composants/footer/footer.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', chargerFooter);
+
+async function chargerFooter() {
+  try {
+    const footer = document.getElementById('footer');
+    const reponseFooter = await fetch('composants/footer/footer.html');
+
+    if (!reponseFooter.ok) {
+      throw new Error('Erreur lors du chargement du footer');
+    }
+
+    const html = await reponseFooter.text();
+    footer.innerHTML = html;
+
+  } catch (erreur) {
+    console.error('Erreur :', erreur);
+  }
+
+}
+
