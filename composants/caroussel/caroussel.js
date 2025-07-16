ac20276699e8ebd25f60cf946cb5210f03b71b6c
Maël Barbé <mael.barbe47@gmail.com>
2025-07-16 16:10:53 +0200

    fix carousel + fix form in com + add img + fix header

--- composants/caroussel/caroussel.js
+++ composants/caroussel/caroussel.js
@@ -1,20 +1,18 @@
-<script>
-    const mainImage = document.getElementById('mainImage');
-    const thumbs = document.querySelectorAll('.thumb');
+const mainImage = document.getElementById('mainImage');
+const thumbs = document.querySelectorAll('.thumb');
 
-    thumbs.forEach(thumb => {
-        thumb.addEventListener('click', () => {
-            const newSrc = thumb.getAttribute('data-src');
-            if (mainImage.src.includes(newSrc)) return; // déjà affichée
+thumbs.forEach(thumb => {
+    thumb.addEventListener('click', () => {
+        const newSrc = thumb.getAttribute('data-src');
+        if (mainImage.src.includes(newSrc)) return; // déjà affichée
 
-            // Ajoute la classe fade-out
-            mainImage.classList.add('fade-out');
+        // Ajoute la classe fade-out
+        mainImage.classList.add('fade-out');
 
-            // Après l'animation, change la source et enlève fade-out
-            setTimeout(() => {
-                mainImage.src = newSrc;
-                mainImage.classList.remove('fade-out');
-            }, 500); // durée identique au CSS (0.5s)
-        });
+        // Après l'animation, change la source et enlève fade-out
+        setTimeout(() => {
+            mainImage.src = newSrc;
+            mainImage.classList.remove('fade-out');
+        }, 500); // durée identique au CSS (0.5s)
     });
-</script>
+});
